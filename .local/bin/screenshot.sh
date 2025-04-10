6296396f4fb876f42872e74f2049c81a8488b798
Xosrov <seyed.alireza.miryazdi@gmail.com>
2025-04-10 11:07:45 +0200

    CHANGE: screenshot file name

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-scrot -s -e 'xclip -selection clipboard -t image/png -i $f' ~/Pictures/Screenshots/scrot-%b%d-%H:%M:%S.png
+scrot -s -e 'xclip -selection clipboard -t image/png -i $f' ~/Pictures/Screenshots/scrot_%b%d-%H_%M_%S.png
