7733cf45f030898259607441226d246f078f912d
Alireza Miryazdi <seyed.alireza.miryazdi@gmail.com>
2025-05-21 09:00:26 +0000

    UPDATE: wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 -- This will hold the configuration
 local config = wezterm.config_builder()
@@ -13,7 +14,44 @@ config.window_padding = {
     bottom = 8
 }
 
--- For exampoe, changing the colourscheme:
+config.scrollback_lines = 10000
+
+config.keys = { -- Close the tab
+{
+    key = 'w',
+    mods = 'CTRL|SHIFT',
+    action = act.CloseCurrentTab {
+        confirm = true
+    }
+}, -- Open a new tab
+{
+    key = 't',
+    mods = 'CTRL|SHIFT',
+    action = act.SpawnTab('CurrentPaneDomain')
+}, -- Split the current pane vertically
+{
+    key = 'd',
+    mods = 'CTRL|SHIFT',
+    action = act.SplitVertical {
+        domain = 'CurrentPaneDomain'
+    }
+}, -- Split the current pane horizontally
+{
+    key = 's',
+    mods = 'CTRL|SHIFT',
+    action = act.SplitHorizontal {
+        domain = 'CurrentPaneDomain'
+    }
+}, {
+    key = 'K',
+    mods = 'CTRL|SHIFT',
+    action = act.Multiple {act.ClearScrollback 'ScrollbackAndViewport', act.SendKey {
+        key = 'L',
+        mods = 'CTRL'
+    }}
+}}
+
+-- Changing the colourscheme:
 config.color_scheme = 'Catppuccin Mocha'
 
 -- And finally, return the configuration to wezterm
